e134561378fb138095060ca6ddf6810c76f92776
Vadim Voitenko <wwoytenko@gmail.com>
2023-04-27 23:43:32 +0300

    * Added uptime profiling to log

--- cmd/greenmask/main.go
+++ cmd/greenmask/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"github.com/rs/zerolog/log"
 	"github.com/wwoytenko/greenfuscator/cmd/greenmask/cmd"
+	"time"
 )
 
 func main() {
+	startTime := time.Now()
 	if err := cmd.Execute(); err != nil {
 		log.Fatal().Err(err).Msg("Fatal")
 	}
+	log.Debug().Msgf("uptime %f", time.Since(startTime).Seconds())
 }
