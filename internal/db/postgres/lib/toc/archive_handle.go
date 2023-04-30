23dd163d4049b7fcaaaa408ee4730a4652b51340
Vadim Voitenko <wwoytenko@gmail.com>
2023-05-01 00:50:43 +0300

    * Implemented backward compatibility with PostgreSQL version either for read or write TOC functions. The write functions only for 15/14 version has been kept

--- internal/db/postgres/lib/toc/archive_handle.go
+++ internal/db/postgres/lib/toc/archive_handle.go
@@ -454,6 +454,206 @@ func (ah *ArchiveHandle) SetEntries(entries []*Entry) {
 	ah.tocList = entries
 }
 
+func (ah *ArchiveHandle) writeHeadV2() error {
+	if err := ah.writeBuf([]byte("PGDMP")); err != nil {
+		return fmt.Errorf("cannot write magic str: %w", err)
+	}
+	if err := ah.writeByte(ah.VersionMajor); err != nil {
+		return fmt.Errorf("cannot write versionMajor: %w", err)
+	}
+
+	if err := ah.writeByte(ah.VersionMinor); err != nil {
+		return fmt.Errorf("cannot write versionMinor: %w", err)
+	}
+
+	if ah.VersionMajor > 1 || (ah.VersionMajor == 1 && ah.VersionMinor > 0) {
+		if err := ah.writeByte(ah.VersionRev); err != nil {
+			return fmt.Errorf("cannot write versionRev: %w", err)
+		}
+	}
+
+	if err := ah.writeByte(byte(ah.IntSize)); err != nil {
+		return fmt.Errorf("cannot write intSize: %w", err)
+	}
+
+	if ah.Version >= BackupVersions["1.7"] {
+		if err := ah.writeByte(byte(ah.OffSize)); err != nil {
+			return fmt.Errorf("cannot write offSize: %w", err)
+		}
+	}
+
+	// TODO: Fixme - I've changed hardcoded ArchTar to ah.Format - it may bring an error
+	if err := ah.writeByte(ah.Format); err != nil {
+		return fmt.Errorf("cannot write format: %w", err)
+	}
+
+	// TODO: discover about compressionNotSet - how it is determining in the C code
+	var compressionNotSet int32 = -1
+	if ah.Version >= BackupVersions["1.15"] {
+		if err := ah.writeInt(compressionNotSet); err != nil {
+			return fmt.Errorf("cannot write CompressionSpec.Algorithm: %w", err)
+		}
+	} else if ah.Version >= BackupVersions["1.2"] {
+		if ah.Version < BackupVersions["1.4"] {
+			if err := ah.writeByte(byte(ah.CompressionSpec.Level)); err != nil {
+				return fmt.Errorf("cannot write CompressionSpec.Algorithm: %w", err)
+			}
+		} else {
+			if err := ah.writeInt(ah.CompressionSpec.Level); err != nil {
+				return fmt.Errorf("cannot write CompressionSpec.Algorithm: %w", err)
+			}
+		}
+	}
+
+	if ah.Version >= BackupVersions["1.4"] {
+		if err := ah.writeInt(ah.CrtmDateTime.TmSec); err != nil {
+			return fmt.Errorf("cannot write TmSec: %w", err)
+		}
+		if err := ah.writeInt(ah.CrtmDateTime.TmMin); err != nil {
+			return fmt.Errorf("cannot write TmMin: %w", err)
+		}
+		if err := ah.writeInt(ah.CrtmDateTime.TmHour); err != nil {
+			return fmt.Errorf("cannot write TmHour: %w", err)
+		}
+		if err := ah.writeInt(ah.CrtmDateTime.TmMday); err != nil {
+			return fmt.Errorf("cannot write TmMday: %w", err)
+		}
+		if err := ah.writeInt(ah.CrtmDateTime.TmMon); err != nil {
+			return fmt.Errorf("cannot write TmMon: %w", err)
+		}
+		if err := ah.writeInt(ah.CrtmDateTime.TmYear); err != nil {
+			return fmt.Errorf("cannot write TmYear: %w", err)
+		}
+		if err := ah.writeInt(ah.CrtmDateTime.TmIsDst); err != nil {
+			return fmt.Errorf("cannot write TmIsDst: %w", err)
+		}
+	}
+
+	if ah.Version >= BackupVersions["1.4"] {
+		if err := ah.writeStr(ah.ArchDbName); err != nil {
+			return fmt.Errorf("cannot write archDbName: %w", err)
+		}
+	}
+
+	if ah.Version >= BackupVersions["1.10"] {
+		if err := ah.writeStr(ah.ArchiveRemoteVersion); err != nil {
+			return fmt.Errorf("cannot write archiveRemoteVersion: %w", err)
+		}
+		if err := ah.writeStr(ah.ArchiveDumpVersion); err != nil {
+			return fmt.Errorf("cannot write archiveDumpVersion: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func (ah *ArchiveHandle) writeTocV2() error {
+	var tocCount = int32(len(ah.tocList))
+
+	if err := ah.writeInt(tocCount); err != nil {
+		return fmt.Errorf("cannot write tocCount: %w", err)
+	}
+
+	for _, te := range ah.tocList {
+		if err := ah.writeInt(te.DumpId); err != nil {
+			panic(fmt.Sprintf("unable to write DumpId: %s", err))
+		}
+
+		if err := ah.writeInt(te.HadDumper); err != nil {
+			panic(fmt.Sprintf("unable to write DataDumper: %s", err))
+		}
+
+		if ah.Version >= BackupVersions["1.8"] {
+			oidStr := strconv.FormatUint(uint64(te.CatalogId.TableOid), 10)
+			if err := ah.writeStr(&oidStr); err != nil {
+				panic(fmt.Sprintf("unable to write TableOid: %s", err))
+			}
+		}
+
+		oidStr := strconv.FormatUint(uint64(te.CatalogId.Oid), 10)
+		if err := ah.writeStr(&oidStr); err != nil {
+			panic(fmt.Sprintf("unable to write Oid: %s", err))
+		}
+
+		if err := ah.writeStr(te.Tag); err != nil {
+			panic(fmt.Sprintf("unable to write Tag: %s", err))
+		}
+		if err := ah.writeStr(te.Desc); err != nil {
+			panic(fmt.Sprintf("unable to write Desc: %s", err))
+		}
+
+		if ah.Version >= BackupVersions["1.11"] {
+			if err := ah.writeInt(te.Section); err != nil {
+				panic(fmt.Sprintf("unable to write Section: %s", err))
+			}
+		}
+
+		if err := ah.writeStr(te.Defn); err != nil {
+			panic(fmt.Sprintf("unable to write Defn: %s", err))
+		}
+		if err := ah.writeStr(te.DropStmt); err != nil {
+			panic(fmt.Sprintf("unable to write DropStmt: %s", err))
+		}
+
+		if ah.Version >= BackupVersions["1.3"] {
+			if err := ah.writeStr(te.CopyStmt); err != nil {
+				panic(fmt.Sprintf("unable to write CopyStmt: %s", err))
+			}
+		}
+
+		if ah.Version >= BackupVersions["1.6"] {
+			if err := ah.writeStr(te.Namespace); err != nil {
+				panic(fmt.Sprintf("unable to write Namespace: %s", err))
+			}
+		}
+
+		if ah.Version >= BackupVersions["1.10"] {
+			if err := ah.writeStr(te.Tablespace); err != nil {
+				panic(fmt.Sprintf("unable to write Tablespace: %s", err))
+			}
+		}
+
+		if ah.Version >= BackupVersions["1.14"] {
+			if err := ah.writeStr(te.Tableam); err != nil {
+				panic(fmt.Sprintf("unable to write Tableam: %s", err))
+			}
+		}
+
+		if err := ah.writeStr(te.Owner); err != nil {
+			panic(fmt.Sprintf("unable ro write Owner: %s", err))
+		}
+
+		if ah.Version >= BackupVersions["1.9"] {
+			tableOidRestoring := "false"
+			if err := ah.writeStr(&tableOidRestoring); err != nil {
+				panic(fmt.Sprintf("unable to write \"false\" value: %s", err))
+			}
+		}
+
+		if ah.Version >= BackupVersions["1.5"] {
+			for _, d := range te.Dependencies {
+				depStr := strconv.FormatInt(int64(d), 10)
+				if err := ah.writeStr(&depStr); err != nil {
+					panic(fmt.Sprintf("unable to write entry dependency value: %s", err))
+				}
+			}
+			/* Terminate List */
+			if err := ah.writeStr(nil); err != nil {
+				panic(fmt.Sprintf("unable to write entry Dependencies list terminator: %s", err))
+			}
+		}
+
+		// TODO: Ensure te.FileName is required for all versions
+		// WriteExtraTocPtr - write filename here
+		if err := ah.writeStr(te.FileName); err != nil {
+			panic(fmt.Sprintf("unable to write FileName: %s", err))
+		}
+
+	}
+
+	return nil
+}
+
 func (ah *ArchiveHandle) writeHead() error {
 
 	if err := ah.writeBuf([]byte("PGDMP")); err != nil {
@@ -492,7 +692,6 @@ func (ah *ArchiveHandle) writeHead() error {
 	if err := ah.writeInt(ah.CrtmDateTime.TmSec); err != nil {
 		return fmt.Errorf("cannot write TmSec: %w", err)
 	}
-
 	if err := ah.writeInt(ah.CrtmDateTime.TmMin); err != nil {
 		return fmt.Errorf("cannot write TmMin: %w", err)
 	}
@@ -683,11 +882,11 @@ func ReadFile(reader io.Reader) (*ArchiveHandle, error) {
 func WriteFile(ah *ArchiveHandle, writer io.Writer) error {
 	ah.destFile = writer
 
-	if err := ah.writeHead(); err != nil {
+	if err := ah.writeHeadV2(); err != nil {
 		return err
 	}
 
-	if err := ah.writeToc(); err != nil {
+	if err := ah.writeTocV2(); err != nil {
 		return err
 	}
 	return nil
