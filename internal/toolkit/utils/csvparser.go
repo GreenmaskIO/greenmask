7ecb5af96b5b9c68e78b7e73c8726bd8757bfd21
Vadim Voitenko <wwoytenko@gmail.com>
2023-08-27 17:01:07 +0300

    * Saved changes

--- internal/toolkit/utils/csvparser.go
+++ internal/toolkit/utils/csvparser.go
@@ -1,1 +1,49 @@
 package utils
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+
+	"github.com/wwoytenko/greenfuscator/internal/toolkit/transformers"
+)
+
+// TODO: It's not a production solution. Real copy parser must be backported.
+// 	We have only two to solve it:
+//		1. Fully backport PostgreSQL COPY TEXT format
+//		2. Implement COPY using CSV format. I suspect it may cause escaping problems, but it is easier
+
+type Csv struct {
+	r      *csv.Reader
+	w      *csv.Writer
+	driver *transformers.Driver
+}
+
+func NewCsv(r io.Reader, w io.Writer, driver *transformers.Driver) *Csv {
+	return &Csv{
+		r:      csv.NewReader(r),
+		w:      csv.NewWriter(w),
+		driver: driver,
+	}
+}
+
+func (c *Csv) Read() (*transformers.Record, error) {
+	values, err := c.r.Read()
+	if err != nil {
+		return nil, fmt.Errorf("cannot read dump line: %w", err)
+	}
+	// TODO: You should not create always a new object instead you must re-use old buffer
+	return transformers.NewRecord(c.driver, values), nil
+}
+
+func (c *Csv) Write(r *transformers.Record) error {
+	res, err := r.Encode(nil)
+	if err != nil {
+		return fmt.Errorf("cannot encode record: %w", err)
+	}
+	if err := c.w.Write(res); err != nil {
+		return fmt.Errorf("unnable to write line: %w", err)
+	}
+	c.w.Flush()
+	return nil
+}
