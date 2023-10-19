ac5fcb05a871ece00710f065eb9d466a9d85f9af
Vadim Voitenko <wwoytenko@gmail.com>
2023-10-19 18:52:38 +0300

    * Added test cmd

--- internal/db/postgres/transformers/test/cmd_test.sh
+++ internal/db/postgres/transformers/test/cmd_test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 while read CMD; do
-    echo "2006-01-02 15:04:05.999999999Z"
+#    echo "2006-01-02 15:04:05.999999999Z"
+    echo '{"9":{"d":"MjAxNy0wNi0xMyAxMToyNDowMCswMg==","n":false},"3":{"d":"MjAxNy0wNi0xMyAxMToyMDowMCswMg==","n":false}}'
 done
