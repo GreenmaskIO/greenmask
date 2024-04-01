ee587fd7b107fc9cca79fb43b06f3b3d75dadfab
Joao Zanutto <jpberno@gmail.com>
2024-04-01 10:46:21 -0700

    adjust playground run script

--- playground/run.sh
+++ playground/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "alias psql='psql -U postgres -d $ORIGINAL_DB_NAME -h playground-db'" >> /root/.bashrc
-echo "alias psql_o='psql -U postgres -d $ORIGINAL_DB_NAME -h playground-db'" >> /root/.bashrc
-echo "alias psql_t='psql -U postgres -d $TRANSFORMED_DB_NAME -h playground-db'" >> /root/.bashrc
-echo "alias cleanup='/var/lib/playground/cleanup.sh'" >> /root/.bashrc
+echo "alias psql='psql -U postgres -d $ORIGINAL_DB_NAME -h playground-db'" >> ~/.bashrc
+echo "alias psql_o='psql -U postgres -d $ORIGINAL_DB_NAME -h playground-db'" >> ~/.bashrc
+echo "alias psql_t='psql -U postgres -d $TRANSFORMED_DB_NAME -h playground-db'" >> ~/.bashrc
+echo "alias cleanup='/var/lib/playground/cleanup.sh'" >> ~/.bashrc
 bash
