73fa03f8c7f3532e14d4677dfb5dcb33e4119340
Vadim Voitenko <wwoytenko@gmail.com>
2025-07-10 21:39:24 +0300

    feat: Mysql streamers impl

---
+++ v1/internal/mysql/dumpers/table_data_writer.go
@@ -0,0 +1,86 @@
+package tablestreamer
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/greenmaskio/greenmask/internal/storages"
+	"golang.org/x/sync/errgroup"
+
+	commonmodels "github.com/greenmaskio/greenmask/v1/internal/common/models"
+	"github.com/greenmaskio/greenmask/v1/internal/common/utils"
+	"github.com/greenmaskio/greenmask/v1/pkg/csv"
+)
+
+type TableDataWriter struct {
+	st        storages.Storager
+	usePgzip  bool
+	fileName  string
+	csvWriter *csv.Writer
+	cw        utils.CountWriteCloser
+	cr        utils.CountReadCloser
+	eg        *errgroup.Group
+	cancel    context.CancelFunc
+}
+
+func NewTableDataWriter(
+	table commonmodels.Table,
+	st storages.Storager,
+	usePgzip bool,
+) *TableDataWriter {
+	fileName := fmt.Sprintf("%s__%s.csv.gz", table.Schema, table.Name)
+	return &TableDataWriter{
+		st:       st,
+		usePgzip: usePgzip,
+		fileName: fileName,
+	}
+}
+
+func (t *TableDataWriter) steam(ctx context.Context) func() error {
+	return func() error {
+		if err := t.st.PutObject(ctx, t.fileName, t.cr); err != nil {
+			return fmt.Errorf("put object: %w", err)
+		}
+		return nil
+	}
+}
+
+func (t *TableDataWriter) Open(ctx context.Context) error {
+	t.cw, t.cr = utils.NewGzipPipe(t.usePgzip)
+	t.csvWriter = csv.NewWriter(t.cw)
+	ctx, t.cancel = context.WithCancel(ctx)
+	t.eg, ctx = errgroup.WithContext(ctx)
+	t.eg.Go(t.steam(ctx))
+	return nil
+}
+
+func (t *TableDataWriter) WriteRow(_ context.Context, row [][]byte) error {
+	if err := t.csvWriter.Write(row); err != nil {
+		return fmt.Errorf("write csv: %w", err)
+	}
+	return nil
+}
+
+func (t *TableDataWriter) Close(ctx context.Context) error {
+	t.csvWriter.Flush()
+	if err := t.cr.Close(); err != nil {
+		return fmt.Errorf("close writer: %w", err)
+	}
+	if err := t.eg.Wait(); err != nil {
+		return fmt.Errorf("wait for stream: %w", err)
+	}
+}
+
+func (t *TableDataWriter) Stat() commonmodels.ObjectStat {
+	if t.cw == nil {
+		panic("writer is not opened")
+	}
+	if t.cr == nil {
+		panic("reader is not opened")
+	}
+	return commonmodels.ObjectStat{
+		Size:           t.cw.GetCount(),
+		CompressedSize: t.cr.GetCount(),
+		FileName:       t.fileName,
+	}
+}
